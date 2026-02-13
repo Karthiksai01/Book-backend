ab42965e1c953c25b3e43b856d5dd270b4b3f546
Karthiksai01 <karthiksaibayya24@gmail.com>
2026-02-13 11:27:53 +0530

    fixed render port issue

--- main.py
+++ main.py
@@ -190,4 +190,12 @@ def delete_document(user_id: str, document_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+
+    port = int(os.environ.get("PORT", 10000))
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port
+    )
